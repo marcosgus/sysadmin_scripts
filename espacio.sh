7b60ac339d3ab9ecc73c599ad2877eb7fe601300
marcosgus <68654936+marcosgus@users.noreply.github.com>
2023-01-11 09:12:58 -0300

    Update espacio.sh
    La info del script #

--- espacio.sh
+++ espacio.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-#Este script se deebe ejecutar en un cronjob, sirve para alertar por almacenamiento si se supera el threshold de 90%
+#Este script se deebe ejecutar en un cronjob, sirve para alertar por almacenamiento si supera el threshold de 90%
 #Me lo pidieron en entrevista tecnica que pase pero luego no me llamaron para el trabajo :(
+#Deben cambiar el usuario root por su usuario
 cd /tmp
 df -h | sort -k 5 -r | tee fs.txt
 find . -name "fs.txt" -type f -exec awk 'NR==1 || $5>=90' {} + >>output.txt
-mail -s 'Alerta Filesystem en servidor rickHunter' root < output.txt
+mail -s 'Alerta espacio en servidor Macross' root < output.txt
 rm /tmp/fs.txt
 rm /tmp/output.txt
